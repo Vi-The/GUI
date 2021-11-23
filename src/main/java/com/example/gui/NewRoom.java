00910ee3b0a4349750bed594a0f8fb34d10c77c2
Harun-Abdi <91126094+Harun-Abdi@users.noreply.github.com>
2021-11-23 14:14:50 +0100

    sfasf

--- src/main/java/com/example/gui/NewRoom.java
+++ src/main/java/com/example/gui/NewRoom.java
@@ -1,4 +1,112 @@
 package com.example.gui;
 
-public class NewRoom {
+import javafx.animation.AnimationTimer;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewRoom implements Initializable {
+
+    private BooleanProperty wPressed = new SimpleBooleanProperty();
+    private BooleanProperty aPressed = new SimpleBooleanProperty();
+    private BooleanProperty sPressed = new SimpleBooleanProperty();
+    private BooleanProperty dPressed = new SimpleBooleanProperty();
+
+    private BooleanBinding keyPressed = wPressed.or(aPressed).or(sPressed).or(dPressed);
+
+    private int movementVariable = 2;
+
+    @FXML
+    private Rectangle shape1;
+
+    @FXML
+    private AnchorPane scene;
+
+    @FXML
+    void start(ActionEvent event) {
+        shape1.setLayoutY(200);
+        shape1.setLayoutX(280);
+    }
+
+    AnimationTimer timer = new AnimationTimer() {
+        @Override
+        public void handle(long timestamp) {
+
+            if(wPressed.get()) {
+                shape1.setLayoutY(shape1.getLayoutY() - movementVariable);
+            }
+
+            if(sPressed.get()){
+                shape1.setLayoutY(shape1.getLayoutY() + movementVariable);
+            }
+
+            if(aPressed.get()){
+                shape1.setLayoutX(shape1.getLayoutX() - movementVariable);
+            }
+
+            if(dPressed.get()){
+                shape1.setLayoutX(shape1.getLayoutX() + movementVariable);
+            }
+        }
+    };
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        movementSetup();
+
+        keyPressed.addListener(((observableValue, aBoolean, t1) -> {
+            if(!aBoolean){
+                timer.start();
+            } else {
+                timer.stop();
+            }
+        }));
+    }
+
+    public void movementSetup(){
+        scene.setOnKeyPressed(e -> {
+            if(e.getCode() == KeyCode.W) {
+                wPressed.set(true);
+            }
+
+            if(e.getCode() == KeyCode.A) {
+                aPressed.set(true);
+            }
+
+            if(e.getCode() == KeyCode.S) {
+                sPressed.set(true);
+            }
+
+            if(e.getCode() == KeyCode.D) {
+                dPressed.set(true);
+            }
+        });
+
+        scene.setOnKeyReleased(e ->{
+            if(e.getCode() == KeyCode.W) {
+                wPressed.set(false);
+            }
+
+            if(e.getCode() == KeyCode.A) {
+                aPressed.set(false);
+            }
+
+            if(e.getCode() == KeyCode.S) {
+                sPressed.set(false);
+            }
+
+            if(e.getCode() == KeyCode.D) {
+                dPressed.set(false);
+            }
+        });
+    }
 }
