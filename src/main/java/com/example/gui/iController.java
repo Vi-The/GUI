460863a8e1debf65d942648c352588989061a2f9
baremikkel <75213352+baremikkel@users.noreply.github.com>
2021-12-01 11:34:43 +0100

    cun

--- src/main/java/com/example/gui/iController.java
+++ src/main/java/com/example/gui/iController.java
@@ -7,6 +7,5 @@ public interface iController{
     Group root = new Group();
     String path = "src/Asset/";
     void start(ActionEvent event);
-    //void addImage(Parent root, int posX, int width, int posY, int height, String folderAndImage) throws FileNotFoundException;
-
+    void addCollision();
 }
