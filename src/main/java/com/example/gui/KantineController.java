d67eb37217fba179aa2414a0830bebaf523a10a9
FrejaStrand <90042340+FrejaStrand@users.noreply.github.com>
2021-12-05 14:53:16 +0100

    Collisions i kantine + fredagsbar
    Mit eneste lille problem er at jeg får fjernet døren når jeg tilføjer collisions til den væg hvor udgangen er.

--- src/main/java/com/example/gui/KantineController.java
+++ src/main/java/com/example/gui/KantineController.java
@@ -29,6 +29,66 @@ public class KantineController  implements Initializable, iController {
 
     @Override
     public void addCollision() {
+        int changer = -30;
+        for (int i = 0; i < 19; i++) {
+            //Outer frame - ensures that we can't go out of the frame.
+            collision.addCollision(-30, changer, STANDARD_LENGTH, STANDARD_LENGTH); //Dette er siden modsat Gydehutten
+            collision.addCollision(690, changer, STANDARD_LENGTH, STANDARD_LENGTH); //Dette er siden ud mod Gydehutten N
+            collision.addCollision(changer, -30, STANDARD_LENGTH, STANDARD_LENGTH); //Dette er siden op mod OUTsideSDU
+            collision.addCollision(changer, 530, STANDARD_LENGTH, STANDARD_LENGTH); //Dette er siden ned mod nedenunder.
+            changer += 40;
+        }
+        /*
+        for (int i = 0; i < 9; i++) { //9 har noget med størrelsen af skærmen at gøre.
+            //To Gydehutten N
+            collision.addCollision(changer,570, STANDARD_LENGTH, STANDARD_LENGTH);
+            collision.addCollision(570, changer + 400, STANDARD_LENGTH, STANDARD_LENGTH);
+            changer +=40;
+        }
+
+         */
+
+
+        //collision.showCollisionAreas(scene);
+
+        collision.addCollision(130, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(90, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(50, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(90, 90, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(10, 290, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(10, 330, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(10, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(170, 330, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(290, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(330, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(370, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(410, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(450, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(490, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(530, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(610, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(530, 250, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(490, 250, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(450, 250, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(410, 250, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(370, 250, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(330, 250, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(290, 250, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(250, 250, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(250, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(290, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(330, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(370, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(410, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(450, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(490, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(530, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(530, 330, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(250, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(450, 450, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(490, 450, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.showCollisionAreas(scene);
+
 
     }
 
@@ -49,6 +109,7 @@ public class KantineController  implements Initializable, iController {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         timer.start();
+        addCollision();
     }
 }
 
--- src/main/java/com/example/gui/NedenunderController.java
+++ src/main/java/com/example/gui/NedenunderController.java
@@ -29,6 +29,59 @@ public class NedenunderController  implements Initializable, iController {
 
     @Override
     public void addCollision() {
+        int changer = -30;
+        for (int i = 0; i < 19; i++) {
+            //Outer frame - ensures that we can't go out of the frame.
+            collision.addCollision(-30, changer, STANDARD_LENGTH, STANDARD_LENGTH); //Dette er siden modsat Gydehutten
+            collision.addCollision(690, changer, STANDARD_LENGTH, STANDARD_LENGTH); //Dette er siden ud mod Gydehutten S
+            collision.addCollision(changer, -30, STANDARD_LENGTH, STANDARD_LENGTH); //Dette er siden op mod Kantinan
+            collision.addCollision(changer, 530, STANDARD_LENGTH, STANDARD_LENGTH); //Dette er siden ned mod bikeshop.
+            changer += 40;
+        }
+
+        //collision.showCollisionAreas(scene);
+
+        collision.addCollision(570, 330, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(530, 330, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(570, 410, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(530, 410, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(490, 450, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(450, 450, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(410, 450, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(490, 410, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(450, 410, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(410, 410, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(410, 370, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(250, 410, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(210, 410, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(170, 410, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(130, 410, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(170, 290, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(130, 290, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(90, 290, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(50, 290, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(50, 210, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(90, 210, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(210, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(250, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(290, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(290, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(330, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(370, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(410, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(450, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(370, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(570, 130, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(570, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(530, 170, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(450, 210, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(410, 210, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(370, 210, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(330, 210, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(290, 210, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(250, 210, STANDARD_LENGTH, STANDARD_LENGTH);
+        collision.addCollision(210, 210, STANDARD_LENGTH, STANDARD_LENGTH);
+        //collision.showCollisionAreas(scene);
 
     }
 
@@ -50,6 +103,7 @@ public class NedenunderController  implements Initializable, iController {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         timer.start();
+        addCollision();
     }
 }
 
