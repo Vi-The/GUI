66a31ae6d0c8bd4322f9b9886a1db84e1617cf0f
baremikkel <75213352+baremikkel@users.noreply.github.com>
2021-12-06 09:58:59 +0100

    l

--- src/main/java/com/example/gui/Buttonlistener.java
+++ src/main/java/com/example/gui/Buttonlistener.java
@@ -2,7 +2,7 @@ package com.example.gui;
 
 public class Buttonlistener {
 
-    private Inventory inventory = Keylistener.inventory;
+    private final Inventory inventory = Keylistener.inventory;
     void getActionForButton(String button) {
         inventory.addInv(button);
     }
