6b6a34e5d06fcd269a26ad929c78b4b1fcabeb65
FrejaStrand <90042340+FrejaStrand@users.noreply.github.com>
2021-12-08 09:08:50 +0100

    Nyt og større map implementeret
    + der er tilføjet navne på rummene for at gøre det mere overskueligt når man ikke kender spillet.

--- src/main/java/com/example/gui/Map.java
+++ src/main/java/com/example/gui/Map.java
@@ -19,7 +19,7 @@ public class Map {
     private Stage stage = new Stage();
 
     public void addImage() throws FileNotFoundException {
-        Image Map = new Image(new FileInputStream("src/Assets/Map til spil v2.png"));
+        Image Map = new Image(new FileInputStream("src/Assets/Map over SDU 3.png"));
         ImageView image = new ImageView(Map);
         image.setX(0);
         image.setY(0);
@@ -30,7 +30,7 @@ public class Map {
     void openMap() {
         try {
             root = new Group();
-            Scene scene = new Scene(root,300,300);
+            Scene scene = new Scene(root,690,690);
             this.scene = scene;
             stage.setTitle("Kort over spillet");
             stage.setScene(scene);
